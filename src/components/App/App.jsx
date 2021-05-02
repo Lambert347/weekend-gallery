d44fbd9a3be9ad68ea3f4d05afbc76fb20663235
Ian Lambert <lambe347@umn.edu>
2021-05-02 11:59:44 -0500

    get request from the client added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GalleryList from '../GalleryList/GalleryList';
 import './App.css';
+import axios from 'axios';
+
+
+
 
 function App() {
+    const [galleryList, setGalleryList] = useState([]);
+
+    const getGallery = () => {
+      axios({
+        method: 'GET',
+        url: '/gallery'
+      })
+        .then((response) => {
+          console.log(response.data)
+          setGalleryList(response.data);
+        })
+        .catch(function (error) {
+          console.log('Error with getting data from server', error);
+        });
+    }
+
+    useEffect( () => {
+      getGallery();
+    }, [])
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <GalleryList />
+        <GalleryList list={galleryList}/>
 
       </div>
     );
