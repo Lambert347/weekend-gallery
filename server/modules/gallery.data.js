2ae8f1aa2852212b214674d19932af43c5273809
Ian Lambert <lambe347@umn.edu>
2021-05-02 16:02:34 -0500

    images added

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,11 +1,16 @@
 const galleryItems = [
     { id: 1, path: 'images/sadie.jpg', description: 'One of the last photos I have of my old childhood pup.', likes: 0 },
-    { id: 2, path: 'insert path here later', description: 'Photo of when I spend a week in Seattle with my friend James', likes: 0},
-    {id: 3, path: 'insert path here later',description: 'Photo of me graduating college.', likes: 0 },
-    {id: 4, path: 'insert path here later', description: 'Photo of my little fluffball.', likes: 0},
-    {id: 5, path: 'insert path here later', description: 'Photo of me at my club at University. Many great memories.', likes: 0},
-    {id: 6, path: 'insert path here later', description: 'Photo of me in switzerland', likes: 0},
-    {id: 7, path: 'insert path here later', description: 'Photo of me at sister\'s wedding.', likes: 0}
+    { id: 2, path: 'images/seattle.jpg', description: 'Photo of when I went to seattle for a week with my friend James.', likes: 0},
+    { id: 3, path: 'images/seattle2.jpg', description: 'Additional photo fo when I went to Seattle for a convention.', likes: 0},
+    {id: 4, path: 'insert path here later',description: 'Photo of me graduating college.', likes: 0 },
+    {id: 5, path: 'images/willow1.jpg', description: 'Photo of my little fluffball.', likes: 0},
+    {id: 6, path: 'images/club.jpg', description: 'Photo of me at my club at University. Many great memories.', likes: 0},
+    {id: 7, path: 'images/swiss.jpg', description: 'Photo of my mountain adventure in Switzerland', likes: 0},
+    {id: 8, path: 'insert path here later', description: 'Photo of me at sister\'s wedding.', likes: 0},
+    { id: 9, path: 'images/willow2.jpg', description: 'Willow\'s first Christmas, and Ian\'s first and only attempt to get her out of the tree', likes: 0},
+    { id: 10, path: 'images/highschool.jpg', description: 'Photo of me in highschool going insane trying to fit all my service hours into one day with my friends.', likes: 0},
+    { id: 11, path: 'images/pumpkin.jpg', description: 'Photo of a random adventure to pick out a pumpkin several years ago.', likes: 0},
+    { id: 12, path: 'images/blizzcon.jpg', description: 'Photo of my first adventure to Blizzcon in California', likes: 0},
 
 ];
 
