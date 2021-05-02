948617574cee1a8668ef171563366d01ea2824bb
Ian Lambert <lambe347@umn.edu>
2021-05-02 11:38:49 -0500

    I THINK (and I cannot stress that enough) I got the gallery item to show properly, will need more testing

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -3,6 +3,9 @@ const galleryItems = [
     { id: 2, path: 'insert path here later', description: 'Photo of when I spend a week in Seattle with my friend James', likes: 0},
     {id: 3, path: 'insert path here later',description: 'Photo of me graduating college.', likes: 0 },
     {id: 4, path: 'insert path here later', description: 'Photo of my little fluffball.', likes: 0},
+    {id: 5, path: 'insert path here later', description: 'Photo of me at my club at University. Many great memories.', likes: 0},
+    {id: 6, path: 'insert path here later', description: 'Photo of me in switzerland', likes: 0},
+    {id: 7, path: 'insert path here later', description: 'Photo of me at sister\'s wedding.', likes: 0}
 
 ];
 
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,9 +1,43 @@
+import {useState} from 'react';
+
+
 function GalleryItem(props){
     const item = props.item;
 
+    const [clicked, setClicked] = useState(false);
+    const [likes, setLikes] = useState(0);
+
+    const renderMessage = () => {
+        if (likes === 1 ){
+            return(<p>This was liked by {likes} people!</p>);
+        }
+        else {
+            return(<p>No likes yet!</p>);
+        }
+    }
+
     return (
         <div>
-            
+            <main>
+                {clicked ?
+                (
+                    <div onClick={ () => {setClicked(true)}}>
+                    {item.path}
+                    <button onClick={() => setLikes(like + 1)}></button>
+                    {likes &&
+                        renderMessage()
+                    }
+                    </div>
+            ) : (
+                    <div onClick={ () => {setClicked(false)}}>
+                        {item.description}
+                        {likes &&
+                            renderMessage()
+                        }
+                    </div>
+                )
+            }
+            </main>
         </div>
-    )
+    );
 }
